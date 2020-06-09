a391ceee48d177ce7150893f24de96d2aa2e635b
Aruna <59650583+aruna1922@users.noreply.github.com>
2020-06-09 11:08:48 +0530

    Update sumOfPowerOfDigits.java

--- 9/sumOfPowerOfDigits.java
+++ 9/sumOfPowerOfDigits.java
@@ -2,16 +2,13 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode{
 public int sumOfPowerOfDigits(int input1){
-      if(input1>9){
-			int sum=0;
-			while(input1!=0){
-				int r1 = input1%10;
-				input1 /= 10;
-				int r2 = input1 %10;
-				sum += Math.pow(r2, r1);
-			}
-			return sum+1;
+		int sum=0;
+		while(input1!=0){
+			int r1 = input1%10;
+			input1 /= 10;
+			int r2 = input1 %10;
+			sum += Math.pow(r2, r1);
 		}
-		return 0;
+		return sum+1;
 	}
 }
