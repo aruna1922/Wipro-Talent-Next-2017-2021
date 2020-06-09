adf73f6357a4d5507a43f7cd6499b0dcaff0284f
Aruna <59650583+aruna1922@users.noreply.github.com>
2020-06-09 11:07:35 +0530

    Create sumOfPowerOfDigits.java

---
+++ 9/sumOfPowerOfDigits.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode{
+public int sumOfPowerOfDigits(int input1){
+      if(input1>9){
+			int sum=0;
+			while(input1!=0){
+				int r1 = input1%10;
+				input1 /= 10;
+				int r2 = input1 %10;
+				sum += Math.pow(r2, r1);
+			}
+			return sum+1;
+		}
+		return 0;
+	}
+}
